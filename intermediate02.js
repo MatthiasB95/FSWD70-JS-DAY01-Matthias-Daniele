e576cf7cf3b111b499a61c5f30462eb3d6b6964c
Matthias Bruckbeck <MBruckbeck@gmx.at>
2019-05-16 10:33:15 +0200

    intermediate02

---
+++ intermediate02.js
@@ -0,0 +1,12 @@
+fruits = ["apple", "banana", "kiwi"];
+fruits.push("orange");
+console.log(fruits);
+fruits.pop("orange");
+console.log(fruits);
+
+animals = ["monkey", "horse", "dog"];
+console.log(animals);
+animals.unshift("cat");
+console.log(animals.sort());
+
+
