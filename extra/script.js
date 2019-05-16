5284df0a99cda5d4c7aaee12043165be5f4e32f9
Daniele Argiolas <danieleargiolas9@gmail.com>
2019-05-16 14:07:06 +0200

    extra 2

--- extra/script.js
+++ extra/script.js
@@ -6,5 +6,27 @@
 
 var statement = "Hey there, i am a javascript developer, and i live in vienna";
 
-document.write(statement.substring( 11, 38));
 
+
+var ijavS = statement.substring( 11, 39);
+
+
+var noFirstLetter = ijavS.slice(1)
+
+var upperCase = ijavS.charAt(0).toUpperCase() + noFirstLetter;
+
+//document.write(upperCase);
+
+
+var CommaToDot = upperCase.replace('\,', '\.' );
+
+document.write(CommaToDot);
+
+var arr = [ "apple", "banana", "kiwi"];
+
+//    output [ "apple", "banana", "strawberry","orange","kiwi"]
+
+arr.splice( 2, 0, "strawberry","orange"); 
+
+
+document.write('<br>'+arr);
