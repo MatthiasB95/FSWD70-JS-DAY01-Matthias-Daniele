7b7fe7e9c9a63cf23340b384a8e9f1a28bc57a52
Matthias Bruckbeck <MBruckbeck@gmx.at>
2019-05-16 11:49:29 +0200

    advanced01

---
+++ challenge01.js
@@ -0,0 +1,9 @@
+arr = [
+["Vienna ","is ", "a ", "nice ", "City."],
+["Vienna ","is ", "a ", "nice ", "City."],
+["Vienna ","is ", "a ", "nice ", "City."],
+["Vienna ","is ", "a ", "nice ", "City."],
+["Vienna ","is ", "a ", "nice ", "City."]
+]
+document.write(arr[0][0], arr[1][1], arr[2][2], arr[3][3], arr[4][4]) ;
+
