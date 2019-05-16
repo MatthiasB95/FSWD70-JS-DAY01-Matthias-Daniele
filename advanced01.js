2bf7068edb6a5efd17b09cff56f6c9fc62948e82
Matthias Bruckbeck <MBruckbeck@gmx.at>
2019-05-16 10:49:47 +0200

    advanced01

---
+++ advanced01.js
@@ -0,0 +1,17 @@
+var a = 7;
+var b = 14;
+var c = "21";
+var d = '36';
+var e = 42;
+var sum = a + b + Number(c)+ Number(d)+e;
+console.log(sum);
+
+var f = '1';
+var g = 15;
+var h = 8;
+var i = "1";
+var mult = Number(f)*g*h*Number(i);
+console.log(mult);
+
+var div = sum/mult;
+document.write(div);
